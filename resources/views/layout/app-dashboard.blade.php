a716ec80a5562e87a112e1f6fceaad151440e0cd
= <josephajibodu@gmail.com>
2022-03-27 15:59:10 +0100

    Dashboard refactoring

--- resources/views/layout/app-dashboard.blade.php
+++ resources/views/layout/app-dashboard.blade.php
@@ -84,38 +84,6 @@
 
     <!-- <script src="./js/dashboard.js')}}"></script> -->
 
-    <script src="{{ asset('assets/js/dashboard.js') }}"></script>
-    <script src="{{ asset('assets/js/scripts.js') }}"></script>
-
-
-    <script src="{{ asset('assets/js/settings.js') }}"></script>
-    <script src="{{ asset('assets/js/quixnav-init.js') }}"></script>
-    <script src="{{ asset('assets/js/styleSwitcher.js') }}"></script>
-    <script src="https://s3.tradingview.com/tv.js')}}"></script>
-    <script>
-        new TradingView.widget({
-                "width": "100%",
-                "height": 460,
-                "symbol": "BITSTAMP:BTCUSD",
-                "interval": "D",
-                "timezone": "Etc/UTC",
-                "theme": "Dark",
-                "style": "1",
-                "locale": "en",
-                "toolbar_bg": "#f1f3f6",
-                "enable_publishing": false,
-                "withdateranges": true,
-                "hide_side_toolbar": false,
-                "allow_symbol_change": true,
-                "show_popup_button": true,
-                "popup_width": "1000",
-                "popup_height": "650",
-                "container_id": "tradingview_e8053"
-            }
-
-        );
-    </script>
-
     {{-- Page Custom Javascript(used by the current page) --}}
     @stack('scripts')
 
