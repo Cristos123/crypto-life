7d22f6c7ff40fe72edb0d2edc0143811952cf3e1
= <josephajibodu@gmail.com>
2022-03-29 08:53:12 +0100

    Payout for logging payment to users

---
+++ database/migrations/2022_03_29_074850_create_payouts_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePayoutsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('payouts', function (Blueprint $table) {
+            $table->id();
+            $table->string('reference');
+            $table->foreignId('user_id')
+                ->nullable()
+                ->constrained()
+                ->cascadeOnUpdate()
+                ->nullOnDelete();
+            $table->unsignedBigInteger('balance');
+            $table->unsignedBigInteger('amount');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('payouts');
+    }
+}
